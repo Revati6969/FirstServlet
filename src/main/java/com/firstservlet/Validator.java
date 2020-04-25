1fbe4762836b2de25b51eac4cf9f562573ca43f7
Revati Tekale <revitekale1910@gmail.com>
2020-04-26 00:30:18 +0530

    ADDED:Regex pattern for valid password

--- src/main/java/com/firstservlet/Validator.java
+++ src/main/java/com/firstservlet/Validator.java
@@ -5,14 +5,21 @@ import java.util.regex.Pattern;
 
 public class Validator {
     String userNamePattern = "^[A-Z][a-z]{2,}$";
+    String passwordPattern = "^(?=.{8})(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]*[^A-Za-z0-9][A-Za-z0-9]*$";
 
     public boolean validator(String pattern, String input) {
-        Pattern p = Pattern.compile(pattern);
-        Matcher matcher = p.matcher(input);
+        Pattern patterns = Pattern.compile(pattern);
+        Matcher matcher = patterns.matcher(input);
         return matcher.matches();
     }
 
-    public boolean userName(String username) {
+    public boolean userNameValidation(String username) {
         return validator(userNamePattern, username);
+    }
+
+    public boolean passwordValidation(String password){
+        if(validator(passwordPattern, password))
+            return true;
+        return false;
     }
 }
